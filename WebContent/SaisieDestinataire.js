cb058db9c63e8a3a0f1b72f0fcfcabcf368799ec
emmanuel.poec09 <emmanuel.poec09@PCP-POE-65.formation.imie>
2016-10-04 09:37:59 +0200

    mardi matin

--- WebContent/SaisieDestinataire.js
+++ WebContent/SaisieDestinataire.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	$("#djclient").on("click", function() {;
+	$("#djClient").on("click", function() {
 		if ($("#afficheRef").css("display") == "block") {
-			$("#afficheRef").css("display", "none")
+			$("#afficheRef").css("display", "none");
 		} else {
-			$("#afficheRef").css("display", "block")
+			$("#afficheRef").css("display", "block");
 		}
 	});
 });
