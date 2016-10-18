31336381825de89fb34ace86fea97adf8692edcc
emmanuel.poec09 <emmanuel.poec09@PCP-POE-65.formation.imie>
2016-10-18 11:21:17 +0200

    mardi 11h

---
+++ WebContent/ReceptionLot.js
@@ -0,0 +1,196 @@
+// declaration du tableau Json de stockage
+var tabJsonStockArticle = [];
+var idclub;
+function activerAjouter() {
+	// Active le bouton
+	$('#valider').prop('disabled', false);
+}
+
+function desactiverAjouter() {
+	// Désactive le bouton
+	$("#valider").prop('disabled', true);
+}
+
+$(document)
+		.ready(
+				function() {
+
+					$("#dateEnvoi").val(moment().format('DD/MM/YYYY'));
+					$(function() {
+						$("#dateEnvoi").datepicker({
+							dateFormat : "dd/mm/yy",
+							showOn : "button",
+							buttonImage : "images/calendar.gif",
+							buttonImageOnly : true,
+							buttonText : "Select date"
+
+						});
+					});
+
+					$('#valider')
+							.click(
+									function() {
+
+										var validerJson = {};
+										// date de creation
+										validerJson.ea_dateCreation = $(
+												'#dateEnvoi').val();
+										// realisé par
+										validerJson.ea_realisePar = $(
+												'#preparePar').val();
+										// numero de colis
+										validerJson.ea_TrackingColis = $(
+												'#suiviColis').val();
+										// nb d'acticle
+										validerJson.ea_nbArticleEnvoyeTotal = tabJsonStockArticle.length;
+										// commentaire
+										validerJson.ea_infoComplementaire = $(
+												'#commentaire').val();
+										// type d'expedition id
+										validerJson.te_id = $('#expedition')
+												.val();
+										// destinataire id
+										validerJson.de_id = idclub;
+										
+										var url = "SaisirExpeditionArticleServlet"
+										$
+												.post(
+														url,
+														validerJson,
+														function(data) {
+
+															var url2 = "SaisirLotArticleServlet";
+
+															for (var k = 0; k < tabJsonStockArticle.length; k++) {
+
+																tabJsonStockArticle[k].id = escape(data);
+
+																$
+																		.post(
+																				url2,
+																				tabJsonStockArticle[k]);
+															}
+
+														});
+											
+											window.location = 'IndexServlet';
+											window.print();
+
+									});
+					desactiverAjouter();
+					// gestion des deletes Edit tableau en bas
+					$(document).on(
+							"click",
+							'#tabArticle a',
+							function() {
+								if ($(this).html() == "Edit") {
+									var select = $(this).closest("tr")
+											.children("td").html();
+									alert(select);
+								}
+								if ($(this).html() == "Delete") {
+									$(this).closest("tr").hide('slow');
+									// Supprimer la ligne dans le tableau json
+									// avec le
+									// champ ref
+									tabJsonStockArticle.splice(
+											tabJsonStockArticle.indexOf($(this)
+													.closest("tr").children(
+															"td").html()), 1);
+									// desactivation du bp valider si tableau
+									// vide
+									$('#afficheNbElement').html("il y a "+tabJsonStockArticle.length+" Article(s)");
+									
+									if (tabJsonStockArticle.length == 0) {
+										desactiverAjouter();
+										$('#afficheNbElement').html("");
+									}
+								}
+							});
+					// connection ajax pour remplissage du tableau
+					$('#ajouter')
+							.click(
+									function() {
+										activerAjouter();
+										var ref = $('#reference').val();
+										var url = "TableauArticleAjaxServlet?ref="
+												+ escape(ref);
+										$
+												.getJSON(
+														url,
+														function(data) {
+															// on test s'il y a
+															// la ref dans la
+															// base sinon msg
+															// d'erreur
+															if (data.sa_ref) {
+																$('#errBas')
+																.html("");
+																tabJsonStockArticle
+																		.push(data);
+																var ligne = '<tr>'
+																		+ '<td>'
+																		+ data.sa_ref
+																		+ '</td>'
+																		+ '<td>'
+																		+ data.mo_nom
+																		+ '</td>'
+																		+ '<td>'
+																		+ data.ta_nom
+																		+ '</td>'
+																		+ '<td><a href="#">Edit</a></td>'
+																		+ '<td><a href="#">Delete</a></td>'
+																		+ '</tr>';
+																$(ligne)
+																		.prependTo(
+																				"#tabArticle");
+																
+																$('#afficheNbElement').html("il y a "+(tabJsonStockArticle.length)+" Article(s)");
+															} else {
+																$('#errBas')
+																		.html(
+																				data);
+															}
+															;
+														});
+
+										$('#reference').val("");
+									});
+					var JsonAutoCompleteClub = [];
+					var url = "AutoCompDestinataireExperdierLot?valeur=";
+
+					$.getJSON(url, function(data) {
+						// console.log(data);
+						for (var k = 0; k < data.length; k++) {
+							JsonAutoCompleteClub.push(data[k].de_nomClub);
+						}
+						var options = {
+							source : JsonAutoCompleteClub,
+							select : function(event, ui) {
+								idclub = data[JsonAutoCompleteClub
+										.indexOf(ui.item.label)].de_id;
+								// console.log(data[JsonAutoCompleteClub.indexOf(ui.item.label)].de_nomContact);
+							}
+						};
+						$('#club').autocomplete(options);
+
+					});
+
+					// autoComplete Reference
+					var JsonAutoCompleteRef = [];
+					var url = "AutoCompRefStockArticle?valeur=";
+
+					$.getJSON(url, function(data) {
+						// console.log(data);
+						for (var k = 0; k < data.length; k++) {
+							// console.log(data[k].sa_ref);
+							JsonAutoCompleteRef.push(data[k].sa_ref);
+						}
+						var options = {
+							source : JsonAutoCompleteRef
+						};
+						$('#reference').autocomplete(options);
+
+					});
+
+				});
---
+++ WebContent/ReceptionLot.jsp
@@ -0,0 +1,156 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="java.util.List"%>
+<%@ page import="fr.creasport.webgestionstock.metier.TypeExpedition"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+<meta charset="UTF-8">
+<script src="jquery-3.1.0.min.js"></script>
+<link rel="stylesheet" href="ReceptionLot.css" />
+<link rel="stylesheet" href="CreaSport.css" />
+<link rel="stylesheet" href="dialClub.css" />
+<link rel="stylesheet"href="jquery-ui.css">
+<script src="jquery-ui.js"></script>
+<script src="ReceptionLot.js"></script>
+<script src="moment.js"></script>
+<script src="Menu.js"></script>
+<script src="dialClub.js"></script>
+
+<title>RECEPTION LOT</title>
+
+</head>
+<body>
+
+	<div id="menuHeader">
+		<div>
+			<div class="header">
+				<div>
+					<img src="images/logoCreasport&Slogan.png" width="363" height="72">
+				</div>
+
+			</div>
+			<div class="header">
+				<div>
+					<nav id="menu"></nav>
+				</div>
+			</div>
+		</div>
+
+		<div class="titre">
+			<h1>Expedier Lot</h1>
+		</div>
+	</div>
+	<hr>
+	<div class="cadre" name="groupe">
+		<div class="conteneur" id="titre1">Information Destinataire</div>
+		<div class="conteneur">
+			<div class="element">
+				<div class="titreInput">Club:</div>
+				<div>
+					<input type="text" name="club" id="club" Placeholder="Club" />
+					<input type="button" value="+" id="addClub" />
+				</div>
+			</div>
+			<div class="vide"></div>
+			<div class="element">
+				<div class="titreInput2"> Préparé par : <input type="text" name="preparePar" id="preparePar"
+					Placeholder="Préparé par" maxlength="05" /></div>
+			</div>
+			<div class="vide"></div>
+			<div class="element">
+				 <div class="titreInput2">Le : <input type="text" name="dateEnvoi" id="dateEnvoi"
+					Placeholder="date" /></div>
+			</div>
+		</div>
+		<div class="conteneur">
+			<div class="element">
+				<div class="titreInput">
+					<label for="expedition">Expedition : </label>
+				</div>
+				<div>
+					<select name="expedition" id="expedition">
+						<%
+							List<TypeExpedition> listeTypesExpeditions = (List<TypeExpedition>) request.getAttribute("listoption");
+					
+						%>
+						<%
+							for (int i = 0, j = listeTypesExpeditions.size(); i < j; i++) {
+								
+						%>
+						
+						<option value="<%=listeTypesExpeditions.get(i).getTe_idString()%>" ><%=listeTypesExpeditions.get(i).getTe_nom()%></option>
+						<%
+							}
+						%>
+					</select>
+				</div>
+			</div>
+		</div>
+
+		<div class="conteneur">
+			<div class="element">
+				<div class="titreInput">N° suivi colis :</div>
+				<div>
+					<input type="text" name="suiviColis" id="suiviColis"
+						Placeholder="N° suvi colis" />
+				</div>
+			</div>
+		</div>
+		<div class="conteneur">
+			<div class="element">
+				<div class="titreInput">
+					<label for="ameliorer">Commentaire : </label>
+				</div>
+				<div>
+					<textarea name="commentaire" id="commentaire"
+						Placeholder="Commentaire"></textarea>
+				</div>
+			</div>
+		</div>
+		<div class="conteneur" id="bouton">
+			<div>
+				<input type="submit" name="valider" id="valider" value="valider" />
+			</div>
+		</div>
+	</div>
+	<div class="titre2"></div>
+	<div class="cadre" name="groupe">
+		<div class="conteneur">Liste des articles</div>
+		<div class="conteneur">
+			<div class="element">
+			<div class="titreInput">Réference :</div>
+				 <input type="text" name="reference" id="reference"
+					Placeholder="Référence" />
+			</div>
+			<div class="element">
+				<input type="submit" name="ajouter" id="ajouter" value="ajouter" />
+			</div>
+			<div id="errBas"></div>
+		</div>
+		<div class="conteneur">
+			<div>
+				<table id="tabArticle" border="1">
+					<thead>
+						<!-- En-tête du tableau -->
+						<tr>
+							<th>Ref article</th>
+							<th>Nom</th>
+							<th>Taille</th>
+							<th>Edit</th>
+							<th>Delete</th>
+						</tr>
+					</thead>
+					<tbody>
+						<!-- Corps du tableau -->
+					</tbody>
+				</table>
+			</div>
+			<div class="vide"></div><div><h1  id="afficheNbElement"></h1></div>
+		</div>
+	</div>
+<%@include file="dialClub.jsp" %>	
+</body>
+</html>
